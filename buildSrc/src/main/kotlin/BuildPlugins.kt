dff6077660bcf3062c9c134626a99094f114b1c7
Tmdhoon2 <hks026naver@gmail.com>
2023-01-21 03:27:12 +0900

    ⚰️ :: git ignore

--- buildSrc/src/main/kotlin/BuildPlugins.kt
+++
@@ -1,5 +0,0 @@
-object BuildPlugins {
-    const val ANDROID_APPLICATION_PLUGIN = "com.android.application"
-    const val KOTLIN_ANDROID_PLUGIN = "org.jetbrains.kotlin.android"
-    const val KOTLIN_KAPT = "kotlin-kapt"
-}
