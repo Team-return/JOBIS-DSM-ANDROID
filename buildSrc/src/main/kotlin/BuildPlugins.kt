1ab6ac5e915f1657bc0fa1a459996b254f833615
Tmdhoon2 <hks026naver@gmail.com>
2023-01-21 05:05:27 +0900

    ⚙️ :: gitignore

---
+++ buildSrc/src/main/kotlin/BuildPlugins.kt
@@ -0,0 +1,11 @@
+object BuildPlugins {
+    const val ANDROID_APPLICATION_PLUGIN = "com.android.application"
+    const val ANDROID_LIBRARY = "com.android.library"
+    const val KOTLIN_ANDROID_PLUGIN = "org.jetbrains.kotlin.android"
+    const val KOTLIN_KAPT = "kotlin-kapt"
+    const val HILT_PLUGIN = "dagger.hilt.android.plugin"
+
+
+    const val ANDROID_GRADLE = "com.android.tools.build:gradle:${Version.ANDROID_GRADLE}"
+    const val KOTLIN_GRADLE = "org.jetbrains.kotlin:kotlin-graldle-plugin:${Version.KOTLIN_GRADLE}"
+}
