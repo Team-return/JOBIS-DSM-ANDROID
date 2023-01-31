7be5ba9a4ee1abc86206927f0b1b09bb47666d24
Tmdhoon2 <hks026naver@gmail.com>
2023-01-31 22:16:20 +0900

    ⚙️ :: compose dependency 추가

--- design-system/build.gradle.kts
+++ design-system/build.gradle.kts
@@ -10,6 +10,9 @@ android {
     defaultConfig {
         minSdk = ProjectProperties.COMPILE_SDK
         targetSdk = ProjectProperties.COMPILE_SDK
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -26,7 +29,18 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = ProjectProperties.JVM_VERSION
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.2.0"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
@@ -37,7 +51,13 @@ dependencies {
     implementation(Dependency.COMPOSE.UI)
     implementation(Dependency.COMPOSE.PREVIEW)
     implementation(Dependency.COMPOSE.MATERIAL)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_ui_version"]}")
+    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_ui_version"]}")
+    implementation("androidx.compose.material:material:1.2.0")
     androidTestImplementation(Dependency.COMPOSE.JUNIT)
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_ui_version"]}")
     debugImplementation(Dependency.COMPOSE.UI_TOOLING)
     debugImplementation(Dependency.COMPOSE.UI_TEST)
 
@@ -47,4 +67,6 @@ dependencies {
     testImplementation(Dependency.TEST.JUNIT)
     androidTestImplementation(Dependency.TEST.EXT_JUNIT)
     androidTestImplementation(Dependency.TEST.ESPRESSO)
+    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_ui_version"]}")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_ui_version"]}")
 }
