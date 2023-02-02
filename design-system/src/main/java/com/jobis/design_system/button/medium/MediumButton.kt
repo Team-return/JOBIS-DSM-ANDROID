c64d5416de6e08d42ec6f610e2e94c7e3d1fb65b
Tmdhoon2 <hks026naver@gmail.com>
2023-02-02 18:29:13 +0900

    ✨ :: medium button

---
+++ design-system/src/main/java/com/jobis/design_system/button/medium/MediumButton.kt
@@ -0,0 +1,49 @@
+package com.jobis.design_system.button.medium
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.jobis.design_system.button.basic.BasicOutLineButton
+import com.jobis.design_system.button.basic.BasicSolidButton
+import com.jobis.design_system.typography.typography
+
+@Composable
+fun MediumSolidButton(
+    onClick: () -> Unit,
+    backgroundColor: Color,
+    textColor: Color,
+    text: String,
+    disable: Boolean,
+) {
+    BasicSolidButton(
+        onClick = onClick,
+        text = text,
+        width = 132,
+        height = 44,
+        roundSize = 8,
+        textStyle = typography.heading6,
+        backgroundColor = backgroundColor,
+        textColor = textColor,
+        disable = disable,
+    )
+}
+
+@Composable
+fun MediumOutLineButton(
+    onClick: () -> Unit,
+    backgroundColor: Color,
+    textColor: Color,
+    text: String,
+    disable: Boolean,
+) {
+    BasicOutLineButton(
+        onClick = onClick,
+        text = text,
+        width = 132,
+        height = 44,
+        roundSize = 8,
+        textStyle = typography.heading6,
+        backgroundColor = backgroundColor,
+        textColor = textColor,
+        disable = disable,
+    )
+}
