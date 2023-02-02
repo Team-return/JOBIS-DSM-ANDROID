9dd3416fea5cfbde634a99346a344b659f71599c
Tmdhoon2 <hks026naver@gmail.com>
2023-02-02 18:28:29 +0900

    ✨ :: large button

---
+++ design-system/src/main/java/com/jobis/design_system/button/large/LargeButton.kt
@@ -0,0 +1,49 @@
+package com.jobis.design_system.button.large
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.jobis.design_system.button.basic.BasicOutLineButton
+import com.jobis.design_system.button.basic.BasicSolidButton
+import com.jobis.design_system.typography.typography
+
+@Composable
+fun LargeSolidButton(
+    onClick: ()->Unit,
+    backgroundColor: Color,
+    textColor: Color,
+    text: String,
+    disable: Boolean,
+){
+    BasicSolidButton(
+        onClick = onClick,
+        text = text,
+        width = 320,
+        height = 48,
+        roundSize = 12,
+        textStyle = typography.heading6,
+        backgroundColor = backgroundColor,
+        textColor = textColor,
+        disable = disable,
+    )
+}
+
+@Composable
+fun LargeOutLineButton(
+    onClick: ()->Unit,
+    backgroundColor: Color,
+    textColor: Color,
+    text: String,
+    disable: Boolean,
+){
+    BasicOutLineButton(
+        onClick = onClick,
+        text = text,
+        width = 320,
+        height = 48,
+        roundSize = 12,
+        textStyle = typography.heading6,
+        backgroundColor = backgroundColor,
+        textColor = textColor,
+        disable = disable,
+    )
+}
