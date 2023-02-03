818d06e087d22871853817187738980c52ee424c
Tmdhoon2 <hks026naver@gmail.com>
2023-02-03 18:59:24 +0900

    ♻️ :: refactoring

--- presentation/src/main/java/com/jobis/jobis_android/MainActivity.kt
+++ presentation/src/main/java/com/jobis/jobis_android/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jobis.jobis_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.jobis.jobis_android.feature.login.LoginScreen
 
 class MainActivity: ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
