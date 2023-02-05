cac4f867cf67e277f53f6bd957ae3793247fbf66
Tmdhoon2 <hks026naver@gmail.com>
2023-02-05 20:18:54 +0900

    ✨ :: SetWindowStatus 함수 구현

--- presentation/src/main/java/com/jobis/jobis_android/MainActivity.kt
+++ presentation/src/main/java/com/jobis/jobis_android/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.jobis.jobis_android
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jobis.design_system.button.basic.direction
+import com.jobis.design_system.color.color
 
-class MainActivity: ComponentActivity(){
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
+        setContent {
+            SetWindowStatus()
         }
     }
+
+    @Composable
+    fun SetWindowStatus(){
+        window.statusBarColor = color.Gray100.toArgb()
+        window.navigationBarColor = color.Gray100.toArgb()
+
+        @Suppress("DEPRECATION")
+        if (MaterialTheme.colors.surface.luminance() > 0.5f) {
+            window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+
+        @Suppress("DEPRECATION")
+        if (MaterialTheme.colors.surface.luminance() > 0.5f) {
+            window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or
+                    View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+    }
+
 }
