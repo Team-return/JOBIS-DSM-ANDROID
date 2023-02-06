f98a2b82d1915270a8bd257cedc3bf50e281cc1a
Tmdhoon2 <hks026naver@gmail.com>
2023-02-06 15:40:05 +0900

    ♻️ :: project route

--- presentation/src/main/java/com/jobis/jobis_android/root/navigation/AuthNavigation.kt
+++ presentation/src/main/java/com/jobis/jobis_android/root/navigation/AuthNavigation.kt
@@ -4,7 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.jobis.jobis_android.feature.SplashScreen
+import com.jobis.jobis_android.root.SplashScreen
 import com.jobis.jobis_android.feature.auth.login.LoginScreen
 
 fun NavGraphBuilder.authNavigation(
