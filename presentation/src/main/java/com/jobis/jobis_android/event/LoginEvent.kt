b1bff5594c9c48e880beb0aef4e301d6eeeda839
Tmdhoon2 <hks026naver@gmail.com>
2023-02-09 02:01:47 +0900

    🛠 :: remove files

--- presentation/src/main/java/com/jobis/jobis_android/event/LoginEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.jobis.jobis_android.event
-
-sealed class LoginEvent {
-    object MoveToMainActivity : LoginEvent()
-    object UnAuthorization : LoginEvent()
-    object NotFound : LoginEvent()
-}
