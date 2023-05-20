cdb3e98f058cde4d170ee2f4ca5b3980c7e85bdf
정승훈 <hks026naver@gmail.com>
2023-05-21 00:48:13 +0900

    🧩 :: HomeScreen 기업 지원 상태 리스트 퍼블리싱

--- presentation/src/main/java/team/retum/jobis_android/feature/home/HomeScreen.kt
+++ presentation/src/main/java/team/retum/jobis_android/feature/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,10 +26,10 @@ import androidx.navigation.NavController
 import com.jobis.jobis_android.R
 import team.retum.jobisui.colors.JobisColor
 import team.retum.jobisui.ui.theme.Body1
+import team.retum.jobisui.ui.theme.Body3
 import team.retum.jobisui.ui.theme.Body4
 import team.retum.jobisui.ui.theme.Caption
 import team.retum.jobisui.ui.theme.Heading3
-import team.retum.jobisui.ui.theme.Heading6
 import team.returm.jobisdesignsystem.image.JobisImage
 import team.returm.jobisdesignsystem.util.JobisSize
 
@@ -36,20 +37,39 @@ import team.returm.jobisdesignsystem.util.JobisSize
 fun HomeScreen(
     navController: NavController,
 ) {
+    // TODO 서버 값으로 리팩토링
+    val tempList = emptyList<String>()
+
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         StudentInformation(
             employmentRate = "32.5",
             major = "major",
         )
-        Spacer(modifier = Modifier.height(28.dp))
-        EmptyComponent()
         Spacer(modifier = Modifier.height(24.dp))
-        MenuCardGroup()
+        // TODO 서버 값으로 리팩토링
+        if (tempList.isNotEmpty()) {
+            CompanyApplyHistoryList(
+                historyList = tempList,
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = JobisColor.Gray200,
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(
+                modifier = Modifier.height(28.dp),
+            )
+            MenuCardGroup()
+        }
     }
+
 }
 
 @Composable
@@ -61,11 +81,11 @@ private fun StudentInformation(
         modifier = Modifier
             .fillMaxWidth()
             .height(98.dp)
-            .shadow(
-                elevation = 48.dp,
-            )
             .padding(
                 horizontal = 14.dp,
+            )
+            .shadow(
+                elevation = 48.dp,
             )
             .clip(
                 shape = RoundedCornerShape(
@@ -127,13 +147,137 @@ private fun StudentInformation(
                 )
             }
         }
-
     }
 }
 
 @Composable
-private fun EmptyComponent() {
+private fun CompanyApplyHistoryList(
+    historyList: List<Any>,
+) {
 
+    Box(
+        modifier = Modifier.padding(
+            horizontal = 30.dp,
+        )
+    ) {
+
+        val size = if (historyList.size >= 2) 2
+        else historyList.size
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(
+                space = 16.dp,
+            )
+        ) {
+            items(
+                count = size,
+            ) { index ->
+                if (index == 0) {
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+                CompanyApplyHistoryItem(
+                    company = historyList[index].toString(),
+                    status = historyList[index].toString(),
+                    date = historyList[index].toString(),
+                )
+                if (index == size - 1) {
+                    Spacer(modifier = Modifier.height(18.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CompanyApplyHistoryItem(
+    company: String,
+    status: String,
+    date: String,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(68.dp)
+            .shadow(
+                shape = RoundedCornerShape(
+                    size = 10.dp,
+                ),
+                elevation = 4.dp,
+            )
+            .clip(
+                shape = RoundedCornerShape(
+                    size = 10.dp,
+                )
+            )
+            .background(
+                color = JobisColor.Gray200,
+            ),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(
+                    horizontal = 18.dp,
+                    vertical = 10.dp,
+                ),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Body3(
+                    text = company
+                )
+                Caption(
+                    text = date,
+                    color = JobisColor.Gray600,
+                )
+            }
+            Row {
+                Caption(
+                    text = stringResource(id = R.string.home_apply_current),
+                )
+                Caption(
+                    text = " $status ",
+                    color = JobisColor.LightBlue,
+                )
+                Caption(
+                    text = stringResource(id = R.string.home_apply_status),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun MenuCardGroup() {
+    Row(
+        modifier = Modifier
+            .height(200.dp)
+            .padding(
+                horizontal = 24.dp,
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Card(
+            modifier = Modifier.weight(0.6f),
+            alignment = Alignment.BottomEnd,
+            text = stringResource(id = R.string.home_do_get_recruitment),
+            drawable = R.drawable.ic_get_recruitment,
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Card(
+            modifier = Modifier.weight(0.4f),
+            alignment = Alignment.BottomCenter,
+            text = stringResource(id = R.string.home_do_get_company),
+            drawable = R.drawable.ic_get_company,
+        )
+    }
+    Spacer(modifier = Modifier.height(10.dp))
 }
 
 @Composable
@@ -163,9 +307,8 @@ private fun Card(
                     vertical = 14.dp,
                 ),
         ) {
-            Heading6(
+            Body1(
                 text = text,
-                color = JobisColor.Blue,
             )
         }
         if (drawable != null) {
@@ -174,31 +317,4 @@ private fun Card(
             )
         }
     }
-}
-
-@Composable
-private fun MenuCardGroup() {
-    Row(
-        modifier = Modifier
-            .height(170.dp)
-            .padding(
-                horizontal = 24.dp,
-            ),
-        horizontalArrangement = Arrangement.SpaceBetween,
-    ) {
-        Card(
-            modifier = Modifier.weight(0.6f),
-            alignment = Alignment.BottomEnd,
-            text = stringResource(id = R.string.home_do_get_recruitment),
-            drawable = R.drawable.ic_get_recruitment,
-        )
-        Spacer(modifier = Modifier.width(12.dp))
-        Card(
-            modifier = Modifier.weight(0.4f),
-            alignment = Alignment.BottomCenter,
-            text = stringResource(id = R.string.home_do_get_company),
-            drawable = R.drawable.ic_get_company,
-        )
-    }
-    Spacer(modifier = Modifier.height(10.dp))
 }
