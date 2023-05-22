049501e9c2c8cb25e71376d055d20649085ec4c2
정승훈 <hks026naver@gmail.com>
2023-05-22 20:19:47 +0900

    🧩 :: RecruitmentListEntity 구현

---
+++ domain/src/main/java/team/retum/domain/entity/RecruitmentListEntity.kt
@@ -0,0 +1,15 @@
+package team.retum.domain.entity
+
+data class RecruitmentListEntity(
+    val recruitmentEntities: List<RecruitmentEntity>,
+)
+
+data class RecruitmentEntity(
+    val recruitId: Int,
+    val companyName: String,
+    val companyProfileUrl: String,
+    val trainPay: Int,
+    val military: Boolean,
+    val totalHiring: Int,
+    val jobCodeList: List<String>,
+)
