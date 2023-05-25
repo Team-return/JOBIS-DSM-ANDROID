b150764139be158885749819abd47be8444919db
정승훈 <hks026naver@gmail.com>
2023-05-26 00:15:31 +0900

    🧩 :: fetchAppliedCompanyHistories Repository 함수 구현

--- data/src/main/java/team/retum/data/repository/ApplicationsRepositoryImpl.kt
+++ data/src/main/java/team/retum/data/repository/ApplicationsRepositoryImpl.kt
@@ -2,6 +2,7 @@ package team.retum.data.repository
 
 import team.retum.data.remote.datasource.declaration.ApplicationsDataSource
 import team.retum.data.remote.response.toEntity
+import team.retum.domain.entity.AppliedCompanyHistoriesEntity
 import team.retum.domain.entity.TotalPassedStudentCountEntity
 import team.retum.domain.repository.ApplicationsRepository
 import javax.inject.Inject
@@ -11,4 +12,7 @@ class ApplicationsRepositoryImpl @Inject constructor(
 ): ApplicationsRepository {
     override suspend fun fetchPassedStudentCount(): TotalPassedStudentCountEntity =
         applicationsDataSource.fetchTotalPassedStudentCount().toEntity()
+
+    override suspend fun fetchAppliedCompanyHistories(): AppliedCompanyHistoriesEntity =
+        applicationsDataSource.fetchAppliedCompanyHistories().toEntity()
 }
--- domain/src/main/java/team/retum/domain/repository/ApplicationsRepository.kt
+++ domain/src/main/java/team/retum/domain/repository/ApplicationsRepository.kt
@@ -1,7 +1,10 @@
 package team.retum.domain.repository
 
+import team.retum.domain.entity.AppliedCompanyHistoriesEntity
 import team.retum.domain.entity.TotalPassedStudentCountEntity
 
 interface ApplicationsRepository {
     suspend fun fetchPassedStudentCount(): TotalPassedStudentCountEntity
+
+    suspend fun fetchAppliedCompanyHistories(): AppliedCompanyHistoriesEntity
 }
