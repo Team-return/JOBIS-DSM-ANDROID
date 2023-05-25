72744f592ef0eb591ec8c11935cb08a511336894
정승훈 <hks026naver@gmail.com>
2023-05-26 00:15:02 +0900

    🧩 :: fetchAppliedCompanyHistories DataSource 함수 구현

--- data/src/main/java/team/retum/data/remote/datasource/declaration/ApplicationsDataSource.kt
+++ data/src/main/java/team/retum/data/remote/datasource/declaration/ApplicationsDataSource.kt
@@ -1,7 +1,10 @@
 package team.retum.data.remote.datasource.declaration
 
+import team.retum.data.remote.response.FetchAppliedCompanyHistoriesResponse
 import team.retum.data.remote.response.FetchTotalPassedStudentCountResponse
 
 interface ApplicationsDataSource {
     suspend fun fetchTotalPassedStudentCount(): FetchTotalPassedStudentCountResponse
+
+    suspend fun fetchAppliedCompanyHistories(): FetchAppliedCompanyHistoriesResponse
 }
--- data/src/main/java/team/retum/data/remote/datasource/implementation/ApplicationsDataSourceImpl.kt
+++ data/src/main/java/team/retum/data/remote/datasource/implementation/ApplicationsDataSourceImpl.kt
@@ -2,6 +2,7 @@ package team.retum.data.remote.datasource.implementation
 
 import team.retum.data.remote.api.ApplicationsApi
 import team.retum.data.remote.datasource.declaration.ApplicationsDataSource
+import team.retum.data.remote.response.FetchAppliedCompanyHistoriesResponse
 import team.retum.data.remote.response.FetchTotalPassedStudentCountResponse
 import team.retum.data.util.HttpHandler
 import javax.inject.Inject
@@ -12,5 +13,10 @@ class ApplicationsDataSourceImpl @Inject constructor(
     override suspend fun fetchTotalPassedStudentCount(): FetchTotalPassedStudentCountResponse =
         HttpHandler<FetchTotalPassedStudentCountResponse>().httpRequest {
             applicationsApi.fetchTotalPassedStudentCount()
+        }.sendRequest()
+
+    override suspend fun fetchAppliedCompanyHistories(): FetchAppliedCompanyHistoriesResponse =
+        HttpHandler<FetchAppliedCompanyHistoriesResponse>().httpRequest {
+            applicationsApi.fetchAppliedCompanyHistories()
         }.sendRequest()
 }
