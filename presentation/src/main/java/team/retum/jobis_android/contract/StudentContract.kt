307f8e87dc5b10dc459fc1ab250afa9942c201f5
정승훈 <hks026naver@gmail.com>
2023-05-26 07:26:11 +0900

    🧩 :: StudentsViewModel 구현

---
+++ presentation/src/main/java/team/retum/jobis_android/contract/StudentContract.kt
@@ -0,0 +1,27 @@
+package team.retum.jobis_android.contract
+
+import team.retum.domain.entity.Department
+import team.retum.jobis_android.util.mvi.Event
+import team.retum.jobis_android.util.mvi.SideEffect
+import team.retum.jobis_android.util.mvi.State
+
+sealed class StudentSideEffect : SideEffect {
+    class SuccessFetchStudentInformation(
+        val studentName: String,
+        val studentGcn: String,
+        val department: Department,
+        val profileImageUrl: String,
+    ) : StudentSideEffect()
+
+    class Exception(
+        val message: String,
+    ): StudentSideEffect()
+}
+
+data class StudentState(
+    val temp: String = "",
+) : State
+
+sealed class StudentEvent : Event {
+    object FetchStudentInformation : StudentEvent()
+}
