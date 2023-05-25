56cde56fe9d90024fa236b84976c2ca08f8931e6
정승훈 <hks026naver@gmail.com>
2023-05-26 07:25:43 +0900

    🧩 :: FetchStudentInformationUseCase 구현

---
+++ domain/src/main/java/team/retum/domain/usecase/FetchStudentInformationUseCase.kt
@@ -0,0 +1,12 @@
+package team.retum.domain.usecase
+
+import team.retum.domain.repository.StudentsRepository
+import javax.inject.Inject
+
+class FetchStudentInformationUseCase @Inject constructor(
+    private val studentsRepository: StudentsRepository,
+) {
+    suspend operator fun invoke() = runCatching{
+        studentsRepository.fetchStudentInformation()
+    }
+}
