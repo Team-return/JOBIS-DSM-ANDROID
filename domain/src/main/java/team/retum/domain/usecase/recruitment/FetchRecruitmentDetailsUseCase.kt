b5262f5cf5ad1baee6b267fc07f75bb6e760c452
정승훈 <hks026naver@gmail.com>
2023-05-29 18:41:43 +0900

    🧩 :: FetchRecruitmentDetails usecase 구현

---
+++ domain/src/main/java/team/retum/domain/usecase/recruitment/FetchRecruitmentDetailsUseCase.kt
@@ -0,0 +1,16 @@
+package team.retum.domain.usecase.recruitment
+
+import team.retum.domain.repository.RecruitmentRepository
+import javax.inject.Inject
+
+class FetchRecruitmentDetailsUseCase @Inject constructor(
+    private val recruitmentRepository: RecruitmentRepository,
+) {
+    suspend operator fun invoke(
+        recruitmentId: Long,
+    ) = runCatching{
+        recruitmentRepository.fetchRecruitmentDetails(
+            recruitmentId = recruitmentId,
+        )
+    }
+}
