f3db528752717a7339741f643f4df1363b8ae210
정승훈 <hks026naver@gmail.com>
2023-06-02 23:24:58 +0900

    🧩 :: MainViewModel 구현

---
+++ presentation/src/main/java/team/retum/jobis_android/viewmodel/main/MainViewModel.kt
@@ -0,0 +1,39 @@
+package team.retum.jobis_android.viewmodel.main
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.runBlocking
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+import team.retum.domain.usecase.user.FetchAutoSignInOptionUseCase
+import team.retum.jobis_android.util.mvi.Event
+import team.retum.jobis_android.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val fetchAutoSignInOptionUseCase: FetchAutoSignInOptionUseCase
+) : BaseViewModel<MainState, MainSideEffect>() {
+
+    override fun sendEvent(event: Event) {}
+
+    override val container = container<MainState, MainSideEffect>(MainState())
+
+    internal fun fetchAutoSignInOption() {
+        runBlocking {
+            fetchAutoSignInOptionUseCase().onSuccess {
+                setAutoSignInOptionState(it)
+            }
+        }
+    }
+
+    private fun setAutoSignInOptionState(
+        autoSignInOption: Boolean,
+    ) = intent {
+        reduce {
+            state.copy(
+                autoSignInOption = autoSignInOption,
+            )
+        }
+    }
+}
