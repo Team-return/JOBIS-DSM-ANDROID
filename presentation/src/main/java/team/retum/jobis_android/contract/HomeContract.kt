5062ddfec490bfe505b4b381c254c5d2f26c9aca
정승훈 <hks026naver@gmail.com>
2023-06-11 20:15:42 +0900

    🛠️ :: HomeViewModel 재구성

---
+++ presentation/src/main/java/team/retum/jobis_android/contract/HomeContract.kt
@@ -0,0 +1,28 @@
+package team.retum.jobis_android.contract
+
+import team.retum.domain.entity.applications.AppliedCompanyHistoryEntity
+import team.retum.domain.entity.applications.StudentCountsEntity
+import team.retum.domain.entity.student.Department
+import team.retum.domain.entity.student.StudentInformationEntity
+import team.retum.jobis_android.util.mvi.SideEffect
+import team.retum.jobis_android.util.mvi.State
+
+data class HomeState(
+    val studentCounts: StudentCountsEntity = StudentCountsEntity(
+        totalStudentCount = 0L,
+        passCount = 0L,
+        approvedCount = 0L,
+    ),
+    val appliedCompanyHistories: List<AppliedCompanyHistoryEntity> = emptyList(),
+    val studentInformation: StudentInformationEntity = StudentInformationEntity(
+        studentName = "",
+        studentGcn = "",
+        department = Department.DEFAULT,
+        profileImageUrl = "",
+    ),
+) : State
+
+sealed class HomeSideEffect : SideEffect {
+    object FetchHomeInformationSuccess : HomeSideEffect()
+    object FetchHomeInformationFailure : HomeSideEffect()
+}
