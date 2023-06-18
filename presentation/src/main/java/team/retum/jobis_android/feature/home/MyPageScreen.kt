6d3902b55cab20887d8096e6b91236c32f644923
정승훈 <hks026naver@gmail.com>
2023-06-18 14:55:23 +0900

    🧩 :: 마이페이지 기능 구현

--- presentation/src/main/java/team/retum/jobis_android/feature/home/MyPageScreen.kt
+++ presentation/src/main/java/team/retum/jobis_android/feature/home/MyPageScreen.kt
@@ -2,23 +2,31 @@ package team.retum.jobis_android.feature.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.jobis.jobis_android.R
+import kotlinx.coroutines.runBlocking
 import team.retum.domain.entity.student.Department
 import team.retum.jobis_android.feature.recruitment.Header
+import team.retum.jobis_android.util.compose.skeleton
+import team.retum.jobis_android.viewmodel.home.HomeViewModel
 import team.retum.jobisui.colors.JobisColor
 import team.returm.jobisdesignsystem.theme.Body2
 import team.returm.jobisdesignsystem.theme.Body4
@@ -27,7 +35,17 @@ import team.returm.jobisdesignsystem.theme.Heading6
 @Composable
 internal fun MyPageScreen(
     navController: NavController,
+    homeViewModel: HomeViewModel = hiltViewModel(),
 ) {
+
+    val state = homeViewModel.container.stateFlow.collectAsState()
+
+    val studentInformation = state.value.studentInformation
+
+    runBlocking {
+        homeViewModel.fetchStudentInformations()
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -41,10 +59,10 @@ internal fun MyPageScreen(
         Header(text = stringResource(id = R.string.bottom_nav_my_page))
         Spacer(modifier = Modifier.height(50.dp))
         UserProfile(
-            profileImageUrl = "",
-            name = "승훈 정",
-            department = Department.AI_SOFTWARE,
-            studentGcn = "2205",
+            profileImageUrl = studentInformation.profileImageUrl,
+            name = studentInformation.studentName,
+            department = studentInformation.department,
+            studentGcn = studentInformation.studentGcn,
         )
 
     }
@@ -57,21 +75,56 @@ private fun UserProfile(
     department: Department,
     studentGcn: String,
 ) {
-    val grade = studentGcn[0]
-    val classRoom = studentGcn[1]
-    val number = studentGcn.substring((if (studentGcn[2].toInt() == 0) 3 else 2)..3)
+
+    var grade = ""
+    var classRoom = ""
+    var number = ""
+
+    if (studentGcn.isNotEmpty()) {
+        grade = studentGcn[0].toString().ifEmpty { "" }
+        classRoom = studentGcn[1].toString().ifEmpty { "" }
+        number = studentGcn.substring((if (studentGcn[2].toInt() == 0) 3 else 2)..3)
+    }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         AsyncImage(
-            modifier = Modifier.size(62.dp),
+            modifier = Modifier
+                .size(62.dp)
+                .skeleton(
+                    show = profileImageUrl.isEmpty(),
+                    shape = CircleShape,
+                ),
             model = profileImageUrl,
             contentDescription = null,
         )
         Spacer(modifier = Modifier.height(6.dp))
-        Heading6(text = name)
-        Body2(text = department.department)
+        Heading6(
+            modifier = Modifier
+                .defaultMinSize(minWidth = 56.dp)
+                .skeleton(show = name.isEmpty()),
+            text = name,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Body2(
+            modifier = Modifier
+                .defaultMinSize(minWidth = 164.dp)
+                .skeleton(show = department == Department.DEFAULT),
+            text = department.department,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
         Body4(
-            text = stringResource(id = R.string.student_gcn, grade, classRoom, number),
+            modifier = Modifier
+                .defaultMinSize(minWidth = 82.dp)
+                .skeleton(
+                    show = grade.isEmpty()
+                ),
+            text = if (grade.isNotEmpty()) stringResource(
+                id = R.string.student_gcn,
+                grade,
+                classRoom,
+                number
+            )
+            else "",
             color = JobisColor.Gray700,
         )
         Spacer(modifier = Modifier.height(80.dp))
