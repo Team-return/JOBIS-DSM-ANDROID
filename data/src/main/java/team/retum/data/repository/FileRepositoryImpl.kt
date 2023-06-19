3384dc7d79bca3bfad759875ebfb2ca17184e4ba
Tmdhoon2 <hks026naver@gmail.com>
2023-06-20 01:11:43 +0900

    🧩 :: getImageMultipart 함수 구현

--- data/src/main/java/team/retum/data/repository/FileRepositoryImpl.kt
+++ data/src/main/java/team/retum/data/repository/FileRepositoryImpl.kt
@@ -1,5 +1,8 @@
 package team.retum.data.repository
 
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import team.retum.data.remote.datasource.declaration.FileDataSource
 import team.retum.data.remote.response.file.toEntity
 import team.retum.domain.entity.FileType
@@ -10,12 +13,19 @@ import javax.inject.Inject
 
 class FileRepositoryImpl @Inject constructor(
     private val fileDataSource: FileDataSource,
-): FileRepository {
+) : FileRepository {
     override suspend fun uploadFile(
         type: FileType,
         files: List<File>,
     ): UploadFileEntity = fileDataSource.uploadFile(
         type = type,
-        files = files,
+        files = files.map { it.getImageMultipart(key = "file") },
     ).toEntity()
 }
+
+private fun File.getImageMultipart(key: String): MultipartBody.Part =
+    MultipartBody.Part.createFormData(
+        name = key,
+        filename = name,
+        body = asRequestBody("multipart/form-data".toMediaType())
+    )
