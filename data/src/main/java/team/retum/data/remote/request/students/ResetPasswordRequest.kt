bfddfe8b3edcf1700d5b6f50217da3c1b25e6a08
정승훈 <hks026naver@gmail.com>
2023-06-30 00:15:41 +0900

    🧩 :: reset password repository 함수 구현

--- data/src/main/java/team/retum/data/remote/request/students/ResetPasswordRequest.kt
+++ data/src/main/java/team/retum/data/remote/request/students/ResetPasswordRequest.kt
@@ -1,8 +1,14 @@
 package team.retum.data.remote.request.students
 
 import com.google.gson.annotations.SerializedName
+import team.retum.domain.param.students.ResetPasswordParam
 
 data class ResetPasswordRequest(
     @SerializedName("current_password") val currentPassword: String,
     @SerializedName("new_password") val newPassword: String,
+)
+
+fun ResetPasswordParam.toRequest() = ResetPasswordRequest(
+    currentPassword = this.currentPassword,
+    newPassword = this.newPassword,
 )
--- data/src/main/java/team/retum/data/repository/StudentsRepositoryImpl.kt
+++ data/src/main/java/team/retum/data/repository/StudentsRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package team.retum.data.repository
 
 import team.retum.data.remote.datasource.declaration.StudentsDataSource
+import team.retum.data.remote.request.students.toRequest
 import team.retum.data.remote.response.students.toEntity
 import team.retum.domain.entity.student.StudentInformationEntity
+import team.retum.domain.param.students.ResetPasswordParam
 import team.retum.domain.repository.StudentsRepository
 import javax.inject.Inject
 
@@ -16,5 +18,13 @@ class StudentsRepositoryImpl @Inject constructor(
         password: String,
     ) {
         studentsDataSource.comparePassword(password = password)
+    }
+
+    override suspend fun resetPassword(
+        resetPasswordParam: ResetPasswordParam,
+    ) {
+        studentsDataSource.resetPassword(
+            resetPasswordRequest = resetPasswordParam.toRequest(),
+        )
     }
 }
