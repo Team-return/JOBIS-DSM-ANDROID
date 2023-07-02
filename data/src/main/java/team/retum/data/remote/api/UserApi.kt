fd0e069b8e9858b301501b765e67531459a8b237
Tmdhoon2 <hks026naver@gmail.com>
2023-07-02 15:57:40 +0900

    🛠️ :: Response<Void> 제거

--- data/src/main/java/team/retum/data/remote/api/UserApi.kt
+++ data/src/main/java/team/retum/data/remote/api/UserApi.kt
@@ -1,13 +1,12 @@
 package team.retum.data.remote.api
 
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Query
-import team.retum.data.remote.request.user.SignInRequest
 import team.retum.data.remote.request.user.SendVerificationCodeRequest
+import team.retum.data.remote.request.user.SignInRequest
 import team.retum.data.remote.request.user.SignUpRequest
 import team.retum.data.remote.response.user.SignInResponse
 import team.retum.data.remote.url.JobisUrl
@@ -33,7 +32,7 @@ interface UserApi {
     suspend fun verifyEmail(
         @Query("email") email: String,
         @Query("auth_code") authCode: String,
-    ): Response<Void>
+    )
 
     @POST(JobisUrl.student)
     suspend fun signUp(
