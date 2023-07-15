deca462760ea1463c016bc137bc0749e66ddc4ef
Tmdhoon2 <hks026naver@gmail.com>
2023-07-15 16:11:33 +0900

    🧩 :: JobisSnackBarHost 구현

---
+++ presentation/src/main/java/team/retum/jobis_android/util/compose/component/ToastLayout.kt
@@ -0,0 +1,24 @@
+package team.retum.jobis_android.util.compose.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun ToastLayout(
+    content: @Composable () -> Unit,
+){
+    Column(
+        modifier = Modifier.padding(
+            top = 48.dp,
+            start = 22.dp,
+            end = 22.dp
+        ),
+        verticalArrangement = Arrangement.Top,
+    ){
+        content()
+    }
+}
