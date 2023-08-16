943d5d97b25ca3736133421f57dd24aa53873347
정승훈 <hks026naver@gmail.com>
2023-08-16 20:17:12 +0900

    🧩 :: editProfileImage datasource 함수 구현

--- data/src/main/java/team/retum/data/remote/datasource/implementation/StudentsDataSourceImpl.kt
+++ data/src/main/java/team/retum/data/remote/datasource/implementation/StudentsDataSourceImpl.kt
@@ -2,6 +2,7 @@ package team.retum.data.remote.datasource.implementation
 
 import team.retum.data.remote.api.StudentsApi
 import team.retum.data.remote.datasource.declaration.StudentsDataSource
+import team.retum.data.remote.request.students.EditProfileImageRequest
 import team.retum.data.remote.request.students.ResetPasswordRequest
 import team.retum.data.remote.response.students.FetchStudentInformationResponse
 import team.retum.data.util.HttpHandler
@@ -9,23 +10,26 @@ import javax.inject.Inject
 
 class StudentsDataSourceImpl @Inject constructor(
     private val studentsApi: StudentsApi,
-): StudentsDataSource {
+) : StudentsDataSource {
     override suspend fun fetchStudentInformation(): FetchStudentInformationResponse =
         HttpHandler<FetchStudentInformationResponse>().httpRequest {
             studentsApi.fetchStudentInformation()
         }.sendRequest()
 
-    override suspend fun comparePassword(
-        password: String,
-    ) = HttpHandler<Unit>().httpRequest {
-        studentsApi.comparePassword(password = password)
-    }.sendRequest()
+    override suspend fun comparePassword(password: String) =
+        HttpHandler<Unit>().httpRequest {
+            studentsApi.comparePassword(password = password)
+        }.sendRequest()
 
-    override suspend fun resetPassword(
-        resetPasswordRequest: ResetPasswordRequest,
-    ) = HttpHandler<Unit>().httpRequest {
-        studentsApi.resetPassword(
-            resetPasswordRequest = resetPasswordRequest,
-        )
-    }.sendRequest()
+    override suspend fun resetPassword(resetPasswordRequest: ResetPasswordRequest) =
+        HttpHandler<Unit>().httpRequest {
+            studentsApi.resetPassword(
+                resetPasswordRequest = resetPasswordRequest,
+            )
+        }.sendRequest()
+
+    override suspend fun editProfileImage(editProfileImageRequest: EditProfileImageRequest) =
+        HttpHandler<Unit>().httpRequest {
+            studentsApi.editProfileImage(editProfileImageRequest = editProfileImageRequest)
+        }.sendRequest()
 }
