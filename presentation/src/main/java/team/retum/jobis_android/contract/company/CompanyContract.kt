f8955f004c587e1f0a0ffb9abf487cf36fd7c223
Tmdhoon2 <hks026naver@gmail.com>
2023-08-19 23:27:03 +0900

    🧩 :: fetchReviewableCompany viewmodel 함수 구현

--- presentation/src/main/java/team/retum/jobis_android/contract/company/CompanyContract.kt
+++ presentation/src/main/java/team/retum/jobis_android/contract/company/CompanyContract.kt
@@ -2,6 +2,7 @@ package team.retum.jobis_android.contract.company
 
 import team.retum.domain.entity.company.CompanyEntity
 import team.retum.domain.entity.company.CompanyDetailsEntity
+import team.retum.domain.entity.company.ReviewableCompanyEntity
 import team.retum.jobis_android.util.mvi.SideEffect
 import team.retum.jobis_android.util.mvi.State
 import java.util.Collections.emptyList
@@ -16,6 +17,7 @@ data class CompanyState(
     var page: Int = 1,
     var name: String? = null,
     var companyId: Int = 0,
+    val reviewableCompanies: List<ReviewableCompanyEntity> = emptyList(),
     var companyDetails: CompanyDetailsEntity = CompanyDetailsEntity(
         address1 = "",
         address2 = null,
@@ -36,5 +38,5 @@ data class CompanyState(
         take = 0.0,
         workerNumber = 0,
         zipCode1 = "",
-    )
+    ),
 ): State
