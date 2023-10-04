51a321088a110f33d872ae1082c5acd49fbd0fc3
Tmdhoon2 <hks026naver@gmail.com>
2023-10-05 01:10:30 +0900

    🛠️ :: pass_count -> passed_count

--- data/src/main/java/team/retum/data/remote/response/application/FetchTotalPassedStudentCountResponse.kt
+++ data/src/main/java/team/retum/data/remote/response/application/FetchTotalPassedStudentCountResponse.kt
@@ -5,7 +5,7 @@ import team.retum.domain.entity.applications.StudentCountsEntity
 
 data class FetchTotalPassedStudentCountResponse(
     @SerializedName("total_student_count") val totalStudentCount: Long,
-    @SerializedName("pass_count") val passCount: Long,
+    @SerializedName("passed_count") val passCount: Long,
     @SerializedName("approved_count") val approvedCount: Long,
 )
 
