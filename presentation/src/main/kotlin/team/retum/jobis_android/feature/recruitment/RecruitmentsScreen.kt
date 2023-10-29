a6ca2bb4b5f21615472c47d42c730dc5de004fd0
정승훈 <hks026naver@gmail.com>
2023-10-30 08:25:46 +0900

    🛠️ :: Recruitment shadow 제거

--- presentation/src/main/kotlin/team/retum/jobis_android/feature/recruitment/RecruitmentsScreen.kt
+++ presentation/src/main/kotlin/team/retum/jobis_android/feature/recruitment/RecruitmentsScreen.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -321,10 +320,6 @@ private fun Recruitment(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .shadow(
-                elevation = 0.dp,
-                shape = ApplyCompaniesItemShape,
-            )
             .clip(shape = ApplyCompaniesItemShape)
             .background(color = JobisColor.Gray100)
             .jobisClickable(
