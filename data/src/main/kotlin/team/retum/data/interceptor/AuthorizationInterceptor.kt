e977655fb22d8b1f6a78dc0b16fafc5d1620c4f3
정승훈 <hks026naver@gmail.com>
2023-10-31 11:19:03 +0900

    🧩 :: joda time

--- data/src/main/kotlin/team/retum/data/interceptor/AuthorizationInterceptor.kt
+++ data/src/main/kotlin/team/retum/data/interceptor/AuthorizationInterceptor.kt
@@ -4,12 +4,12 @@ import com.jobis.data.BuildConfig
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import org.joda.time.LocalDateTime
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import team.retum.data.remote.api.AuthApi
 import team.retum.data.remote.url.JobisUrl
 import team.retum.data.storage.UserDataStorage
-import java.time.LocalDateTime
 import javax.inject.Inject
 
 class AuthorizationInterceptor @Inject constructor(
