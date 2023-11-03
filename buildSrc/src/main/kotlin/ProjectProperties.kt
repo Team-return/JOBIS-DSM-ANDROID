8054625eca9e133ab5670076ccc6b48e4fc14cad
Tmdhoon2 <hks026naver@gmail.com>
2023-11-03 17:03:46 +0900

    🗂️ :: v1.0.4

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -8,8 +8,8 @@ object ProjectProperties {
     const val COMPILE_SDK = 33
     const val MIN_SDK = 24
 
-    const val VERSION_CODE = 12
-    const val VERSION_NAME = "1.0.3"
+    const val VERSION_CODE = 13
+    const val VERSION_NAME = "1.0.4"
 
     const val TEST_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 
