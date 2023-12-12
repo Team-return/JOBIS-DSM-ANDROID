f6df179b862d63777ca0c35071a7e7fa32cd11af
Tmdhoon2 <hks026naver@gmail.com>
2023-12-12 14:40:32 +0900

    chore :: project dependencies version update

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -9,22 +9,22 @@ object Version {
     const val ANDROID_GRADLE = "7.2.1"
     const val KOTLIN_GRADLE = "1.6.10"
 
-    const val COMPOSE_LIFECYCLE = "2.5.1"
-    const val COMPOSE_ACTIVITY = "1.6.1"
-    const val COMPOSE_MATERIAL = "1.3.1"
-    const val COMPOSE_NAVIGATION = "2.5.3"
-    const val COMPOSE_NAVIGATION_HILT = "1.0.0"
-    const val COMPOSE_FLOW_LAYOUT = "0.30.1"
-    const val COMPOSE_NAVIGATION_ANIMATION = "0.30.1"
-    const val COLLECT_AS_STATE_WITH_LIFECYCLE = "2.6.0-alpha04"
+    const val COMPOSE_LIFECYCLE = "2.6.2"
+    const val COMPOSE_ACTIVITY = "1.8.1"
+    const val COMPOSE_MATERIAL = "1.5.4"
+    const val COMPOSE_NAVIGATION = "2.7.5"
+    const val COMPOSE_NAVIGATION_HILT = "1.1.0"
+    const val COMPOSE_FLOW_LAYOUT = "0.32.0"
+    const val COMPOSE_NAVIGATION_ANIMATION = "0.32.0"
+    const val COLLECT_AS_STATE_WITH_LIFECYCLE = "2.6.2"
 
-    const val SPLASH_SCREEN = "1.0.0"
+    const val SPLASH_SCREEN = "1.0.1"
 
-    const val ORBIT_MVI = "6.0.0"
+    const val ORBIT_MVI = "6.1.0"
 
     const val DESIGN_SYSTEM = "v1.2.3"
 
-    const val SHIMMER = "1.0.5"
+    const val SHIMMER = "1.2.0"
 
     const val JODA_TIME = "2.12.5"
 }
