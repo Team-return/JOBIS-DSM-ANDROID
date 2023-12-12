341a2e69a109ab8f96d3435a6a0ddd21827d790c
정승훈 <hks026naver@gmail.com>
2023-12-12 21:45:58 +0900

    feat :: implement in app update

--- presentation/src/main/kotlin/team/retum/jobis_android/MainActivity.kt
+++ presentation/src/main/kotlin/team/retum/jobis_android/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.jobis.jobis_android.R
 import dagger.hilt.android.AndroidEntryPoint
 import team.retum.jobis_android.feature.auth.resetpassword.ResetPasswordViewModel
@@ -21,7 +24,6 @@ import team.retum.jobis_android.util.compose.component.JobisSnackBarHost
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
     private val mainViewModel by viewModels<MainViewModel>()
     private val signUpViewModel by viewModels<SignUpViewModel>()
     private val resetPasswordViewModel by viewModels<ResetPasswordViewModel>()
@@ -31,6 +33,7 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkAppUpdate()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         setContent {
@@ -64,6 +67,24 @@ class MainActivity : ComponentActivity() {
             Toast.makeText(this, getText(R.string.back_handler), Toast.LENGTH_SHORT).show()
         } else {
             finish()
+        }
+    }
+
+    private fun checkAppUpdate() {
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener {
+            val isUpdateAvailable = it.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+
+            if (isUpdateAvailable && it.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                appUpdateManager.startUpdateFlowForResult(
+                    it,
+                    AppUpdateType.FLEXIBLE,
+                    this,
+                    0,
+                )
+            }
         }
     }
 }
