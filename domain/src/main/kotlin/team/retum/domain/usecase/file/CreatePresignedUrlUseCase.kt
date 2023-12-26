d939ae433347a54e58b39ba05bfed45609455701
정승훈 <hks026naver@gmail.com>
2023-12-26 19:40:24 +0900

    feat :: implement create presigned url usecase

---
+++ domain/src/main/kotlin/team/retum/domain/usecase/file/CreatePresignedUrlUseCase.kt
@@ -0,0 +1,15 @@
+package team.retum.domain.usecase.file
+
+import team.retum.domain.param.files.PresignedUrlParam
+import team.retum.domain.repository.FileRepository
+import javax.inject.Inject
+
+class CreatePresignedUrlUseCase @Inject constructor(
+    private val fileRepository: FileRepository,
+) {
+    suspend operator fun invoke(
+        presignedUrlParam: PresignedUrlParam,
+    ) = kotlin.runCatching {
+        fileRepository.createPresignedUrl(presignedUrlParam = presignedUrlParam)
+    }
+}
