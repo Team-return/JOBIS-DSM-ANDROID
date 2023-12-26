76aef8432fd45b3f78d5d3fcc23fc409f5ac38c2
Tmdhoon2 <hks026naver@gmail.com>
2023-12-26 19:30:59 +0900

    feat :: implement file api create presigned url function

--- data/src/main/kotlin/team/retum/data/remote/api/FileApi.kt
+++ data/src/main/kotlin/team/retum/data/remote/api/FileApi.kt
@@ -1,10 +1,13 @@
 package team.retum.data.remote.api
 
 import okhttp3.MultipartBody
+import retrofit2.http.Body
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Query
+import team.retum.data.remote.request.files.CreatePresignedUrlRequest
+import team.retum.data.remote.request.files.CreatePresignedUrlResponse
 import team.retum.data.remote.response.file.UploadFileResponse
 import team.retum.data.remote.url.JobisUrl
 import team.retum.domain.entity.FileType
@@ -16,4 +19,9 @@ interface FileApi {
         @Query("type") type: FileType,
         @Part file: List<MultipartBody.Part>,
     ): UploadFileResponse
+
+    @POST(JobisUrl.Files.presigned)
+    suspend fun createPresignedUrl(
+        @Body createPresignedUrlRequest: CreatePresignedUrlRequest,
+    ): CreatePresignedUrlResponse
 }
