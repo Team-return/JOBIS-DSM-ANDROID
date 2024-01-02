f30c1a31580a484fe0e06eac5a87c48bdb6c84cf
Tmdhoon2 <hks026naver@gmail.com>
2024-01-02 19:07:47 +0900

    refactor :: add PlatformType to SignInRequest

--- data/src/main/kotlin/team/retum/data/remote/request/user/SignInRequest.kt
+++ data/src/main/kotlin/team/retum/data/remote/request/user/SignInRequest.kt
@@ -1,8 +1,10 @@
 package team.retum.data.remote.request.user
 
 import com.google.gson.annotations.SerializedName
+import team.retum.domain.enums.PlatformType
 
 data class SignInRequest(
     @SerializedName("account_id") val accountId: String,
     @SerializedName("password") val password: String,
+    @SerializedName("platform_type") val platformType: PlatformType = PlatformType.ANDROID,
 )
