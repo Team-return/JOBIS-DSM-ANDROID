819a4641562f806f09b5d0530c210d687c14fa68
Tmdhoon2 <hks026naver@gmail.com>
2024-01-16 20:49:36 +0900

    chore :: use java 18

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -39,8 +39,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
         jvmTarget = ProjectProperties.JVM_VERSION
