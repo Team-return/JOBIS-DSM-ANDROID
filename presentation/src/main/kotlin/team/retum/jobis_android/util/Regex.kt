9fa4c8d89007761569022b5e29007e7758154bcb
정승훈 <hks026naver@gmail.com>
2024-01-29 11:34:37 +0900

    refactor :: fix password regex

--- presentation/src/main/kotlin/team/retum/jobis_android/util/Regex.kt
+++ presentation/src/main/kotlin/team/retum/jobis_android/util/Regex.kt
@@ -1,6 +1,6 @@
 package team.retum.jobis_android.util
 
 object Regex {
-    const val PASSWORD = "^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%^&*])[a-z0-9!@#\$%^&*]{8,16}\$"
+    const val PASSWORD = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#\$%^&*])[a-zA-Z\\d!@#\$%^&*]{8,16}\$"
     const val EMAIL = "^.+@dsm.hs.kr$"
 }
